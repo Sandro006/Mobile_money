66c8e2fcd05101adcf97f1e48295bf55a753d739
Ivo Ianteherana <nyavoivorandrianarisoa@gmail.com>
2026-07-20 13:48:43 +0300

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -25,3 +25,27 @@ $routes->get('/gain', [GainController::class, 'index']);
 
 $routes->get('/situation', [SituationController::class, 'index']);
 $routes->get('/situation/detail/(:num)', [SituationController::class, 'detail']);
+// Auth par numéro
+$routes->get('/login', 'LoginController::index');
+$routes->post('/login', 'LoginController::submit');
+$routes->get('/logout', 'LoginController::logout');
+
+// Zone client (protégée)
+$routes->get('/client', 'ClientController::index');
+
+// Opérations
+
+// Depot
+$routes->get('operation/page-depot', 'OperationController::pageDepot');
+$routes->post('operation/depot', 'OperationController::depot');
+
+//  Retrait
+$routes->get('operation/page-retrait', 'OperationController::pageRetrait');
+$routes->post('operation/retrait', 'OperationController::retrait');
+
+//Transfer
+$routes->get('operation/page-transfert', 'OperationController::pageTransfert');
+$routes->post('operation/transfert', 'OperationController::transfert');
+
+// Historique
+$routes->get('client/historique', 'ClientController::historique');
